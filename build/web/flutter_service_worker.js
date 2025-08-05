02b536a265c2c14e806fb63e8adbed1236782a13
db <dbdev52@gmail.com>
2025-08-05 13:25:21 +0200

    Update web build with correct base-href for GitHub Pages

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -128,7 +128,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "588632a03a8f28a32e2527c41da7cff5
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "efa923611da9ad6e0c7547ed06aa24b2",
+"flutter_bootstrap.js": "f0b6b1363538b2fd0b50bb523c2a491f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
